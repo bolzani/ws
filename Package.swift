e1aab428b10d1482884cea490c2680e712102128
Felipe Alves <falves@gazeus.com>
2019-09-25 14:42:38 -0300

    Fixing Package.swift

--- Package.swift
+++ Package.swift
@@ -1,31 +1,17 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(
-    name: "ws",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ws",
-            targets: ["ws"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/bolzani/Arrow", from: "5.0.0"),
-        .package(url: "https://github.com/bolzani/then", from: "5.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.0.0-rc.2")),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ws",
-            dependencies: [],
-            path: "ws"),
-        .testTarget(
-            name: "wsTests",
-            dependencies: ["ws"],
-            path: "wsTests"),
-    ]
-)
+let package = Package(name: "ws",
+                      platforms: [.iOS(.v10)],
+                      products: [.library(name: "ws",
+                                          targets: ["ws"])],
+                      dependencies: [.package(url: "https://github.com/bolzani/Arrow", from: "5.0.0"),
+                                     .package(url: "https://github.com/bolzani/then", from: "5.0.0"),
+                                     .package(url: "https://github.com/Alamofire/Alamofire", .exact("4.9.0"))],
+                      targets: [.target(name: "ws",
+                                        dependencies: ["then", "Arrow", "Alamofire"],
+                                        path: "ws"),
+                                .testTarget(name: "wsTests",
+                                            dependencies: ["ws"],
+                                            path: "wsTests")],
+                      swiftLanguageVersions: [.v5])
