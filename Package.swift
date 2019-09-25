0184d297e0f62def87674a9896a6558e40794d9a
Felipe Alves <falves@gazeus.com>
2019-09-25 09:50:22 -0300

    Fixing Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/bolzani/Arrow", from: "5.0.0"),
         .package(url: "https://github.com/bolzani/then", from: "5.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.0.0-rc.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
